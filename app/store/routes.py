0751390762c442e0bda7bad09ca217bfd9d9b6e6
Joel Flores <joel.flores3000@protonmail.com>
2023-01-11 18:57:17 -0400

    agregando manejo de error 404 para usuarios que intenten ingresar a otrasrutas denegadas

--- app/store/routes.py
+++ app/store/routes.py
@@ -1,6 +1,6 @@
 #importar la configurcion del blueprint
 from . import store
-from flask import session
+from flask import session, abort
 from app.auth.routes import login_required
 #funcion para la ruta de index
 from .index import logic_index
@@ -9,6 +9,8 @@ def requeriment():
     user = session.get('user')
     if user['position'] != 2:
         session.clear()
+        abort(404)
+        
 """
 pagina pricipal del almacen"""
 @store.route('/')
