7e372ac073c9ff2405e5655c1b81852a1d555e16
Joel Flores <joel.flores3000@protonmail.com>
2023-01-11 18:42:38 -0400

    cambiando el formato de request get a getlist para no tener error con la session

--- app/store/store_tech/tech_withdraw_material/request_reel_withdraw.py
+++ app/store/store_tech/tech_withdraw_material/request_reel_withdraw.py
@@ -2,8 +2,9 @@ from flask import request
 def request_reel_withdraw(reels):
     request_id_withdraw = list()
     for reel in reels:
-        bool_value = request.form.get(reel['serial'], False)
-        print(bool_value)
+        bool_value = True
+        if request.form.getlist(reel['serial'], False) == []:
+            bool_value = False
         request_id_withdraw.append({'id': reel["id"],'bool': bool_value})
         
     return request_id_withdraw
